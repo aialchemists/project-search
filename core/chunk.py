ebf9e3eefc5a467bedeeaea800389ecbf8df240a
sreenaths <somarajapuram@gmail.com>
2023-09-12 00:01:46 -0700

    Improving chunkify implimentation - better modularity

--- core/chunk.py
+++ core/chunk.py
@@ -1,5 +1,4 @@
-# pip install numpy spacy
-# python -m spacy download en_core_web_sm
+from typing import List
 
 import numpy as np
 import spacy
@@ -8,14 +7,14 @@ import math
 # Load the Spacy model
 nlp = spacy.load('en_core_web_lg')
 
-def process(text):
+def get_sentences(text):
     doc = nlp(text)
     sents = list(doc.sents)
     vecs = np.stack([sent.vector / sent.vector_norm for sent in sents])
 
     return sents, vecs
 
-def cluster_text(sents, vecs, threshold):
+def get_clusters(sents, vecs, threshold):
     clusters = [[0]]
     for i in range(1, len(sents)):
         if np.dot(vecs[i], vecs[i-1]) < threshold:
@@ -24,42 +23,36 @@ def cluster_text(sents, vecs, threshold):
 
     return clusters
 
-def degree_to_threshold(degree):
+def degree_to_threshold(degree:float) -> float:
     radian = degree * math.pi / 180
     threshold = math.cos(radian)
     return threshold
 
-def chunkify(text, degree = 45):
+def get_sentence_clusters(text, degree) -> List[str]:
+    # Process the chunk
+    sents, vecs = get_sentences(text)
+
+    # Cluster the sentences
+    threshold = degree_to_threshold(degree)
+    clusters = get_clusters(sents, vecs, threshold)
+
+    sentence_clusters = []
+    for cluster in clusters:
+        sentence_clusters.append(' '.join([sents[i].text for i in cluster]))
+
+    return sentence_clusters
+
+def chunkify(text, degree: float = 45, recursion_level = 1) -> List[str]:
     # Initialize the clusters lengths list and final texts list
     chunks = []
 
-    # Process the chunk
-    threshold = degree_to_threshold(degree)
-    sents, vecs = process(text)
-
-    # Cluster the sentences
-    clusters = cluster_text(sents, vecs, threshold)
-
-    for cluster in clusters:
-        cluster_txt = ' '.join([sents[i].text for i in cluster])
-        cluster_len = len(cluster_txt)
-
+    sentence_clusters = get_sentence_clusters(text, degree)
+    for sentence_cluster in sentence_clusters:
         # Check if the cluster is too longs
-        if cluster_len > 3000:
-            threshold = degree_to_threshold(degree * 0.5)
-            sents_div, vecs_div = process(cluster_txt)
-            reclusters = cluster_text(sents_div, vecs_div, threshold)
-
-            for subcluster in reclusters:
-                div_txt = ' '.join([sents_div[i].text for i in subcluster])
-                div_len = len(div_txt)
-
-                if div_len > 3000:
-                    continue
-
-                chunks.append(div_txt)
-
+        if len(sentence_cluster) > 3000 and recursion_level >= 1:
+            split_chunks = chunkify(sentence_cluster, degree * 0.5, recursion_level - 1)
+            chunks.extend(split_chunks)
         else:
-            chunks.append(cluster_txt)
+            chunks.append(sentence_cluster)
 
     return chunks
