cd7114d7dcafeb6cb6d3b027665bb6c41ac4806f
sreenaths <somarajapuram@gmail.com>
2023-09-19 01:20:18 -0700

    Removing dummy serahc response

--- server.py
+++ server.py
@@ -18,24 +18,6 @@ try:
 except Exception as exc:
   log.error("Exception while initialising extract pipeline", exc)
 
-results = [
-  {
-    "text": "Myra was next to him, her ear to the ground, listening for the gallop of horses' hoofs. I got up and went over to them.",
-    "file_path": "http://localhost:8000/files/data/The day's play.pdf",
-    "score": 0.75
-  },
-  {
-    "text": "I took the Queen Anne staircase on my—in the proper stalking position. I moved very slowly, searching for spoor. Half-way down the stairs my back fin slipped and I shot over the old oak at a tremendous pace, landing in the hall like a Channel swimmer. Looking up, I saw Thomas in front of me.",
-    "file_path": "http://localhost:8000/files/data/The day's play.pdf",
-    "score": 0.6
-  },
-  {
-    "text": "Pars in gramineis exercent membra palæstris, Contendunt ludo, et fulvâ luctantur arenâ: Pars pedibus plaudunt choreas, et carmina dicunt.---- Arma procul, currusque virûm miratur inanes.",
-    "file_path": "http://localhost:8000/files/data/The chase.txt",
-    "score": 0.45
-  }
-]
-
 @app.get("/", include_in_schema=False)
 async def root_api():
     response = RedirectResponse(url='/docs')
