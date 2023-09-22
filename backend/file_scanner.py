f128e48a3b5587f03be4c44dd1ab21f32a91b221
sreenaths <somarajapuram@gmail.com>
2023-09-22 02:51:49 -0700

    Renaming extract_pipeline to file_scanner

---
+++ backend/file_scanner.py
@@ -0,0 +1,15 @@
+from utils.logger import log
+import os
+
+from tasks.extract import parse_task
+
+def scan_local_dir(directory_path):
+    if os.path.isdir(directory_path):
+        file_paths = [os.path.join(directory_path, filename) for filename in os.listdir(directory_path) if os.path.isfile(os.path.join(directory_path, filename))]
+        for path in file_paths:
+            if not os.path.basename(path).startswith("."):
+                parse_task.delay(path)
+    else:
+        log.error(f"'{directory_path}' is not a valid directory path.")
+
+scan_local_dir("./data/")
