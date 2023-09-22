1cb65fcba0f7f25c7df4f0a75f7a3398bf0f2487
sreenaths <somarajapuram@gmail.com>
2023-09-22 03:13:59 -0700

    Improved logging in file_scanner

--- backend/file_scanner.py
+++ backend/file_scanner.py
@@ -4,11 +4,14 @@ import os
 from tasks.extract import parse_task
 
 def scan_local_dir(directory_path):
+    log.info(f"Scaning files in dir {directory_path}")
     if os.path.isdir(directory_path):
         file_paths = [os.path.join(directory_path, filename) for filename in os.listdir(directory_path) if os.path.isfile(os.path.join(directory_path, filename))]
         for path in file_paths:
             if not os.path.basename(path).startswith("."):
                 parse_task.delay(path)
+
+        log.info(f"Found {len(file_paths)} files. Tasks scheduled.")
     else:
         log.error(f"'{directory_path}' is not a valid directory path.")
 
